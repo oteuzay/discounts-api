33429ea457f129f52e4fc084ab02571b3bad10ed
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-05 01:12:14 +0300

    feat: integrate sequelize for sync. and start server

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const app = require("./src/app");
 
 const config = require("./src/config/config");
 
+const sequelize = require("./src/config/database");
+
 const logger = require("./src/utils/logger");
 
-app.listen(config.PORT, () => {
-  logger.info(`Server is running on port ${config.PORT}.`);
-});
+/* Synchronizing the Sequelize models with the database and starting the server. */
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(config.PORT, () => {
+      logger.info(`Server is running on port ${config.PORT}.`);
+    });
+  })
+  .catch((err) => {
+    logger.error(err);
+  });
