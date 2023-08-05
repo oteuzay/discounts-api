7f34bc6d7129dcf0977be595a215c12f0b010167
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-05 17:42:14 +0300

    feat: create a user model

---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+/* The code is defining a Sequelize model called "User" with the specified attributes. */
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "User",
+  },
+});
+
+module.exports = User;
