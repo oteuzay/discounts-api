7a933eb6aee280ac1a3330083415b052b687d989
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-06 04:33:48 +0300

    feat: add secret and database variables

--- src/config/config.js
+++ src/config/config.js
@@ -4,4 +4,10 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || "*",
   NODE_ENV: process.env.NODE_ENV || "Development",
+  SECRET: process.env.SECRET,
+  DB: process.env.DB,
+  USERNAME: process.env.DB_USERNAME,
+  PASSWORD: process.env.DB_PASSWORD,
+  HOST: process.env.DB_HOST || "localhost",
+  DIALECT: process.env.DB_DIALECT || "postgres",
 };
